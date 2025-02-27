605d6fa08e4ab5b78e232ae0efa345355ad68304
dulcegit <dulcemunive4@gmail.com>
2025-02-26 21:27:47 -0600

    Actualizando proyecto

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let Amigos = [];
+function agregarAmigo(){
+  
+    let nombre = document.getElementById ("amigo").value 
+    if (nombre == "")  { 
+        alert ("por favor, ingresa tu nombre");
+        
+    } else { 
+        Amigos.push (nombre) 
+        document.querySelector ('input').value = "";
+        mostrarAmigo();
+                }
+     
+}
+function mostrarAmigo (){
+    let lista = document.getElementById ("listaAmigos")
+    lista.innerHTML = ""
+    for (let index = 0; index < Amigos.length; index++) {
+        let li = document.createElement ('li')
+        li.textContent = Amigos [index]
+        lista.appendChild (li)
+
+        
+    }
+}
+function sortearAmigo(){
+    if (Amigos == "") {
+        alert ( "Ingresar amigos");
+        return;
+
+    }
+    let indiceAleatorio = Math.floor(Math.random() * Amigos.length);
+    let amigoSeleccionado = Amigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML = `<li>🎉 Amigo secreto: ${amigoSeleccionado}</li>`;
+}
+
